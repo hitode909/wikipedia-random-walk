85075da9ef99d05a0e146721dc51ae9431eb7965
hitode909 <hitode909@gmail.com>
2014-05-18 15:17:52 +0900

    最初のキーワードなくても動くように

--- walk.rb
+++ walk.rb
@@ -7,10 +7,6 @@ $:.unshift(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'wikipedia-random-walk'
 
-if ARGV.empty?
-  warn "usage: bundle exec sample.rb (KEYWORD)"
-end
-
 KEYWORD = ARGV.first
 
 walker = WikipediaRandomWalker.new(KEYWORD)
